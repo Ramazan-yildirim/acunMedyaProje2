a8db8e49b95362e046b933a925b4c7951f2c682e
ramazanyildirim <ramazanyldr0103@gmail.com>
2025-02-01 22:40:06 +0300

    öğrencinin devamsızlıktan veya not ortalamasından geçme veya kalma durumu kontrol edildi ve buna göre kullanıcıya mesaj verildi.

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from pathlib import Path
 # Dosya yolunu 
 dosya_yolu = Path(r"C:\Users\ramaz\Desktop\akademiqProje2\data.json")
 
-# Dosyayı açıp JSON verisini yükleyin
+# Dosyayı açıp JSON verisini yüklendi
 with open(dosya_yolu, "r") as dosya:
     veri = json.load(dosya)
 
 ders_notlari = veri.get("ders_notlari", [])
+devamsizlik_sayısı = veri.get("devamsizlik")
 
 toplam_not = 0
 not_sayısı = 0
@@ -21,4 +22,12 @@ for index, ders in enumerate(ders_notlari):
 
 ortalama = toplam_not / not_sayısı
 
-print(ortalama)
+#öğrencinin kalma veya geçme durumu kontrol edildi
+if 0 >= ortalama > 50 and 0 >= devamsizlik_sayısı < 10:
+    print("öğrenci sınıfı geçti")
+elif 0 >= devamsizlik_sayısı > 10:
+    print("öğrenci devamsızlıktan kaldı")
+elif 0 >= ortalama < 50:
+    print("öğrenci dersten kaldı")
+else:
+    print("geçersiz giriş")
