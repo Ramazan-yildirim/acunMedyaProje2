e64ba60f5ca09f010a1e2744f47103587557817b
ramazanyildirim <ramazanyldr0103@gmail.com>
2025-02-01 23:00:01 +0300

    kullanıcıdan ad ve soyad verileri alındı. geçersiz veri girişleri kontrol edildi.

--- app.py
+++ app.py
@@ -9,25 +9,34 @@ with open(dosya_yolu, "r") as dosya:
     veri = json.load(dosya)
 
 ders_notlari = veri.get("ders_notlari", [])
-devamsizlik_sayısı = veri.get("devamsizlik")
+devamsizlik_sayisi = veri.get("devamsizlik")
+ogrenci_ad = veri.get("ad", "Bilinmiyor")
+ogrenci_soyad = veri.get("soyad", "Bilinmiyor")
 
 toplam_not = 0
-not_sayısı = 0
+not_sayisi = 0
 
 # ders_notlari indekslerinde dolaş
 for index, ders in enumerate(ders_notlari):
+    #ders notlarının sıfırdan küçük olma durumu kontrol edildi
+    if ders < 0:
+        print("geçersiz not girişi")
+        exit()
     toplam_not += ders
-    not_sayısı += index
+    not_sayisi += index
 
 
-ortalama = toplam_not / not_sayısı
+ortalama = toplam_not / not_sayisi
+
+#devamsızlık durumunun sıfırdan küçük olma durumu kontrol edildi
+if devamsizlik_sayisi < 0:
+    print("geçersiz devamsızlık girişi")
+    exit()
 
 #öğrencinin kalma veya geçme durumu kontrol edildi
-if 0 >= ortalama > 50 and 0 >= devamsizlik_sayısı < 10:
-    print("öğrenci sınıfı geçti")
-elif 0 >= devamsizlik_sayısı > 10:
-    print("öğrenci devamsızlıktan kaldı")
-elif 0 >= ortalama < 50:
-    print("öğrenci dersten kaldı")
-else:
-    print("geçersiz giriş")
+if ortalama > 50 and devamsizlik_sayisi < 10:
+    print(f"{ogrenci_ad} {ogrenci_soyad} sınıfı geçti")
+elif devamsizlik_sayisi > 10:
+    print(f"{ogrenci_ad} {ogrenci_soyad} devamsızlıktan kaldı")
+elif  ortalama < 50:
+    print(f"{ogrenci_ad} {ogrenci_soyad} dersten kaldı")
